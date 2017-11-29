ff31b81d2c9e41795a4c96ef41cf90407997a687
jinsabrina <33047923+jinsabrina@users.noreply.github.com>
2017-11-29 03:46:05 -0800

    add part 3

---
+++ part3.sql
@@ -0,0 +1,102 @@
+-- Test might not have been ran correctly
+-- Does Message Rate translate to real significance, does other metrics also improve
+-- Is data recorded correctly
+
+ SELECT c.experiment,
+       c.experiment_group,
+       c.users,
+       c.total_treated_users,
+       ROUND(c.users/c.total_treated_users,4) AS treatment_percent,
+       c.total,
+       ROUND(c.average,4)::FLOAT AS average,
+       ROUND(c.average - c.control_average,4) AS rate_difference,
+       ROUND((c.average - c.control_average)/c.control_average,4) AS rate_lift,
+       ROUND(c.stdev,4) AS stdev,
+       ROUND((c.average - c.control_average) /
+          SQRT((c.variance/c.users) + (c.control_variance/c.control_users))
+        ,4) AS t_stat,
+       (1 - COALESCE(nd.value,1))*2 AS p_value
+  FROM (
+SELECT *,
+       MAX(CASE WHEN b.experiment_group = 'control_group' THEN b.users ELSE NULL END) OVER () AS control_users,
+       MAX(CASE WHEN b.experiment_group = 'control_group' THEN b.average ELSE NULL END) OVER () AS control_average,
+       MAX(CASE WHEN b.experiment_group = 'control_group' THEN b.total ELSE NULL END) OVER () AS control_total,
+       MAX(CASE WHEN b.experiment_group = 'control_group' THEN b.variance ELSE NULL END) OVER () AS control_variance,
+       MAX(CASE WHEN b.experiment_group = 'control_group' THEN b.stdev ELSE NULL END) OVER () AS control_stdev,
+       SUM(b.users) OVER () AS total_treated_users
+  FROM (
+SELECT a.experiment,
+       a.experiment_group,
+       COUNT(a.user_id) AS users,
+       AVG(a.metric) AS average,
+       SUM(a.metric) AS total,
+       STDDEV(a.metric) AS stdev,
+       VARIANCE(a.metric) AS variance
+  FROM (
+SELECT ex.experiment,
+       ex.experiment_group,
+       ex.occurred_at AS treatment_start,
+       u.user_id,
+       u.activated_at,
+       COUNT(DISTINCT DATE_TRUNC('day',e.occurred_at)) AS metric
+  FROM (SELECT user_id,
+               experiment,
+               experiment_group,
+               occurred_at
+          FROM tutorial.yammer_experiments
+         WHERE experiment = 'publisher_update'
+       ) ex
+  JOIN tutorial.yammer_users u
+    ON u.user_id = ex.user_id
+  JOIN tutorial.yammer_events e
+    ON e.user_id = ex.user_id
+   AND e.occurred_at >= ex.occurred_at
+   AND e.occurred_at < '2014-07-01'
+   AND e.event_type = 'engagement'
+   AND e.event_name = 'login'
+ GROUP BY 1,2,3,4,5
+       ) a
+ GROUP BY 1,2
+       ) b
+       ) c
+  LEFT JOIN benn.normal_distribution nd
+    ON nd.score = ABS(ROUND((c.average - c.control_average)/SQRT((c.variance/c.users) + (c.control_variance/c.control_users)),3))
+	
+-- Check other events such as like_message, view_inbox, and login to see if same trend applies, and it appears that trend does appear to those events as well
+
+SELECT a.experiment,
+       a.experiment_group,
+       DATE_TRUNC('week', a.activated),
+       COUNT(a.user_id) AS users,
+       AVG(a.metric) AS average,
+       SUM(a.metric) AS total
+  FROM (
+SELECT ex.experiment,
+       ex.experiment_group,
+       ex.occurred_at AS treatment_start,
+       u.user_id,
+       u.activated_at AS activated,
+       COUNT(DISTINCT DATE_TRUNC('day',e.occurred_at)) AS metric
+  FROM (SELECT user_id,
+               experiment,
+               experiment_group,
+               occurred_at
+          FROM tutorial.yammer_experiments
+         WHERE experiment = 'publisher_update'
+       ) ex
+  JOIN tutorial.yammer_users u
+    ON u.user_id = ex.user_id
+  JOIN tutorial.yammer_events e
+    ON e.user_id = ex.user_id
+   AND e.occurred_at >= ex.occurred_at
+   AND e.occurred_at < '2014-07-01'
+   AND e.event_type = 'engagement'
+   AND e.event_name = 'view_inbox'
+   AND u.created_at > '2013-07-01'
+ GROUP BY 1,2,3,4,5
+       ) a
+ GROUP BY 1,2,3
+ 
+ -- Checking if there is any bias users in experiment, it looks like control group has alot of new users, and looks like it is skewing the data
+
+ -- Recommendation is to randomly distribute users into control and test group and rerun the experiment
